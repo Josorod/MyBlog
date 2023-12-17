4a19f7ad7704d0c251ed09dd4cff6bb9ca63abc1
roman <roman@DESKTOP-NNCD647>
2023-12-17 14:21:00 +0200

    Add project files.

---
+++ Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBlog.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        [Required,
+         MaxLength(length:50)]
+        public string PostTitle { get; set; }
+        [Required,
+         MaxLength(length:500)]
+        public string PostBody { get; set; }
+        public List<string>? PostImages { get; set; }
+        public List<string>? VideoPlayer { get; set; }
+        [Required, DataType(DataType.DateTime)]
+        public DateTime CreatedTime { get; set; }
+        [Required, DataType(DataType.DateTime)]
+        public DateTime UpdatedTime { get; set; }
+    }
+}
